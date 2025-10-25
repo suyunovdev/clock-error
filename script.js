9137b4b4c9bad7a5bb466d52f4ab596b7d6eb82a
suyunovdev <153995037+suyunovdev@users.noreply.github.com>
2025-10-25 15:40:10 +0500

    finish

--- script.js
+++ script.js
@@ -1,51 +1,40 @@
-// // setTimeout(() => {
-// //   alert("This message appears after 2 seconds.");
-// // }, 5000);
+// setTimeout(() => {
+//   alert("This message appears after 2 seconds.");
+// }, 5000);
 
-// let intervalId = setInterval(function () {
+// let intervalId = setInterval(() => {
 //   console.log("This message appears every 1 second.");
 // }, 1000);
 
 // // To stop the interval after some time
-// setTimeout(function () {
+// setTimeout( ()=> {
 //   clearInterval(intervalId);
 //   console.log("Interval stopped.");
 // }, 5000);
 
-// let date = new Date();
-// console.log(date);
-// console.log(date.getFullYear());
-// console.log(date.getMonth());
-// console.log(date.getDay());
-// console.log(date.getHours());
-// console.log(date.getMinutes());
-
-let hour = document.getElementById("hour");
-let minute = document.getElementById("minute");
-let second = document.getElementById("second");
-setInterval(() => {
-  let date = new Date();
-  let h = date.getHours();
-  let m = date.getMinutes();
-  let s = date.getSeconds();
-  hour.innerHTML = h;
-  minute.innerHTML = m;
-  second.innerHTML = s;
-}, 1000);
+let date = new Date();
+console.log(date);
+console.log(date.getFullYear());
+console.log(date.getMonth());
+console.log(date.getDate());
+console.log(date.getHours());
+console.log(date.getMinutes());
 
 let year = document.getElementById("year");
 let month = document.getElementById("month");
 let day = document.getElementById("day");
-setInterval(() => {
-  let date = new Date();
-  let y = date.getFullYear();
-  let mo = date.getMonth() + 1;
-  let d = date.getDate();
-  year.innerHTML = y;
-  month.innerHTML = mo;
-  day.innerHTML = d;
-}, 1000);
+
+year.innerHTML = date.getFullYear();
+month.innerHTML = date.getMonth();
+day.innerHTML = date.getDate();
+
+let hour = document.getElementById("hour");
+let minute = document.getElementById("minute");
+let second = document.getElementById("second");
 
 setInterval(() => {
-  alert("Tugnani bosing");
-}, 10);
+  let date = new Date();
+  hour.innerHTML = date.getHours();
+  minute.innerHTML = date.getMinutes();
+  second.innerHTML = date.getSeconds();
+}, 1000);
